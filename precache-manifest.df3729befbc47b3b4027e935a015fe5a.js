93fd482307413f911a0ed0f517ec8e1adf08687b
Dilan Livera <dylan_yahampath@yahoo.com>
2019-10-21 10:49:50 +1100

    Updates

---
+++ precache-manifest.df3729befbc47b3b4027e935a015fe5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3b5d2beeb324514a72ad9a795ebd09",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "6654a31a91ffbdcf3b28",
+    "url": "/dad-jokes/static/css/main.6fa7be00.chunk.css"
+  },
+  {
+    "revision": "98edf6c540136b081c42",
+    "url": "/dad-jokes/static/js/2.04f38eb6.chunk.js"
+  },
+  {
+    "revision": "6654a31a91ffbdcf3b28",
+    "url": "/dad-jokes/static/js/main.759290b4.chunk.js"
+  },
+  {
+    "revision": "fdbd532c9e02b3f10882",
+    "url": "/dad-jokes/static/js/runtime-main.079c7ed8.js"
+  }
+]);
